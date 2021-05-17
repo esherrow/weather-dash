12977503575e621b4f802481d574f52d20fd6597
Yoga Cats <esherrow77@gmail.com>
2021-05-16 20:32:53 -0400

    added fetch

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+var searchCity = "muncie" //document.querySelector("form-control");
+
+fetch(
+    'http://api.openweathermap.org/data/2.5/forecast?q='+searchCity+'&units=imperial&cnt=40&appid=650dce3c894fd8e8f9f02e1e10ba6800'
+  )
+    .then(function(forecast) {
+      return forecast.json();
+    })
+    .then(function(forecast) {
+      for(i=0; i<40;) {
+        console.log(forecast.list[i].dt_txt);
+        //console.log(forecast.list[i].main.temp);
+        //console.log(forecast.list[i].main.humidity);
+        //console.log(forecast.list[i].weather[0].icon);
+        //console.log(forecast.list[i].wind.speed);
+        i=i+8;
+        }
+    });
+
+fetch("http://api.openweathermap.org/data/2.5/weather?q="+searchCity+"&units=imperial&appid=650dce3c894fd8e8f9f02e1e10ba6800")
+.then(function(current){
+    return current.json();
+})
+.then(function(current){
+    console.log(current.main.temp);
+    console.log(current.weather[0].icon);
+    console.log(current.wind.speed);
+    console.log(current.main.humidity);
+})
