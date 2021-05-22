c68b0f0096a4a2c7f5952c241bf3c4daca1cb2f5
Yoga Cats <esherrow77@gmail.com>
2021-05-22 10:52:47 -0400

    fix date issues and color

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,26 +14,25 @@ fetch(
             var iconForecast= document.querySelector("icon");
             var forecastIcon= document.createElement('img');
             forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/"+forecast.list[i].weather[0].icon+"@2x.png");
-   
+            //var icon = iconForecast.appendChild(forecastIcon)
+
             lookAhead.append(`
-        <div class="card col bg-dark text-white me-2">
-        <div class="card-header fw-bolder">
-          ${forecast.list[i].dt_txt}
-        </div>
-        <ul class="list-group list-group-flush ">
-            <li class="list-group-item icon bg-dark text-white"></li>
-            <li class="list-group-item temp bg-dark text-white">Temp: ${forecast.list[i].main.temp} F</li>
-            <li class="list-group-item wind bg-dark text-white">Wind Speed: ${forecast.list[i].wind.speed} mph</li>
-            <li class="list-group-item humidity bg-dark text-white">Humidity: ${forecast.list[i].main.humidity}%</li>
-          </ul>
-        </div>`);  
+                <div class="card col bg-info text-white ms-3">
+                <div class="card-header fw-bolder">
+                    ${moment(forecast.list[i].dt_txt).format("L")}
+                </div>
+                <ul class="list-group list-group-flush ">
+                    <li class="list-group-item icon bg-info text-white">${document.querySelector("icon")}</li>
+                    <li class="list-group-item temp bg-info text-white">Temp: ${forecast.list[i].main.temp} F</li>
+                    <li class="list-group-item wind bg-info text-white">Wind Speed: ${forecast.list[i].wind.speed} mph</li>
+                    <li class="list-group-item humidity bg-info text-white">Humidity: ${forecast.list[i].main.humidity}%</li>
+                </ul>
+                </div>`);  
         
-        
-        console.log(forecast.list[i].dt_txt);
-        //console.log(forecast.list[i].main.temp);
-        //console.log(forecast.list[i].main.humidity);
-        console.log(forecast.list[i].weather[0].icon);
-        //console.log(forecast.list[i].wind.speed);
+        //console.log(forecast);
+        console.log(moment(forecast.list[i].dt_txt).format("L"));
+        //console.log(forecast.list[i].weather[0].icon);
+       
         i=i+8;
         }
     });
@@ -57,7 +56,7 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q="+searchCity+"&units=imp
     currentTemp.textContent= "Temp: "+current.main.temp+" F";
     currentWind.textContent="Wind: "+current.wind.speed+" mph";
     currentHumidity.textContent="Humidity: "+current.main.humidity+"%";
-    currentDay.textContent= searchCity+"  "+moment().format("L")+"  ";
+    currentDay.textContent= searchCity+"  ("+moment().format("L")+")  ";
     currentDay.appendChild(currentIcon);
 
     fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=hourly,daily,minutely,alerts&units=imperial&appid=650dce3c894fd8e8f9f02e1e10ba6800")
@@ -71,11 +70,11 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q="+searchCity+"&units=imp
         currentUV.textContent=index.current.uvi;
 
         if(index.current.uvi < 3){
-            currentUV.setAttribute("class", "bg-success rounded-pill badge")
+            currentUV.setAttribute("class", "bg-success badge")
         }else if(index.current.uvi >= 6){
-            currentUV.setAttribute("class", "bg-danger rounded-pill badge")
+            currentUV.setAttribute("class", "bg-danger badge")
         }else{
-            currentUV.setAttribute("class", "bg-warning rounded-pill badge")
+            currentUV.setAttribute("class", "bg-warning badge")
         }
         console.log(index.current.uvi);
     })
